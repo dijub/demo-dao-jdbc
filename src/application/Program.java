988a35425932670852cda72d2f984f109abf478b
diego-insaurralde <diego_insaurralde@live.com>
2024-06-29 22:25:53 -0300

    Create Department class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import model.entities.Department;
+
 /**
  * Program
  */
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello Dijub");
+        Department obj = new Department(1, "books");
+        System.out.println(obj);
+
     }
 }
