387970abe22b512b83d0067da64788ef472f365a
diego-insaurralde <diego_insaurralde@live.com>
2024-06-30 19:30:25 -0300

    Implement find by id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,7 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 /**
@@ -13,10 +10,11 @@ import model.entities.Seller;
 public class Program {
 
     public static void main(String[] args) {
-        Department obj = new Department(1, "books");
-        Seller obj2 = new Seller(1, "Diego", "Diego@gmail.com", LocalDate.now(), 4444.44, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
